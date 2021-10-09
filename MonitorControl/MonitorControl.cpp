e3966283c34ec3274b70e8d3f15a6b78249f6169
ManiFast <62830326+Stas-inside@users.noreply.github.com>
2021-10-09 20:58:46 +0500

    Add files via upload

---
+++ MonitorControl/MonitorControl.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <Windows.h>
+//#include <stdafx.h>
+
+using namespace std;
+
+int main()
+{
+	Beep(400, 1000);
+	SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+	Sleep(1000);
+	SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)-1);
+	Beep(400, 300);
+
+	return 0;
+}
